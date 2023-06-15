f00bed014dfd0b123639c547065b935690c2ea1d
alexaoliveira2000 <77057098+alexaoliveira2000@users.noreply.github.com>
2023-06-15 22:32:44 +0100

    Update glove.js

--- glove.js
+++ glove.js
@@ -8,14 +8,13 @@ let getLinks = async function (page) {
     let isValidLink = function (link) {
         return link && link.startsWith("/wiki/") && !link.includes(":") && path.extname(link) === '' && link !== "Main_Page";
     }
-
     let response = await axios.get(page);
     const $ = cheerio.load(response.data);
     let links = []
     $('a').each(function () {
         const href = $(this).attr('href');
         if (isValidLink(href))
-            links.push(href.split("/")[2]);
+            links.push(href.split("/")[2].toLowerCase());
     });
     return links;
 }
@@ -38,18 +37,14 @@ let getWordSimilarity = function (glove, word1, word2) {
     if(!dimensionsWord1 || !dimensionsWord2)
         return 0;
 
-    // Create matrix representations of your word vectors
     const vectorWord1 = new Matrix([dimensionsWord1]);
     const vectorWord2 = new Matrix([dimensionsWord2]);
 
-    // Calculate the dot product of the vectors
     const dotProduct = vectorWord1.mmul(vectorWord2.transpose()).get(0, 0);
 
-    // Calculate the magnitudes of the vectors
     const magnitude1 = vectorWord1.norm();
     const magnitude2 = vectorWord2.norm();
 
-    // Calculate the cosine similarity
     const similarity = dotProduct / (magnitude1 * magnitude2);
 
     return similarity;
@@ -59,22 +54,22 @@ let play = async function (sourcePage, destinationPage) {
     let glove = await readGloVeFile("C:\\Users\\Alex\\Desktop\\glove-wiki\\glove.6B.50d.txt");
     let pagesPath = [sourcePage];
     let limit = 100;
-    while (sourcePage.toLowerCase() != destinationPage && pagesPath.length < limit) {
+    while (sourcePage != destinationPage && pagesPath.length < limit) {
         let pageLink = "https://en.wikipedia.org/wiki/" + sourcePage
         console.log("Requesting page: " + pageLink);
         let links = await getLinks(pageLink);
         sourcePage = links[0];
         let highestSimilarity = 0;
         for (const link of links) {
-            if (pagesPath.includes(link.toLowerCase()))
+            if (pagesPath.includes(link))
                 continue
-            let similarity = getWordSimilarity(glove, link.toLowerCase(), destinationPage);
+            let similarity = getWordSimilarity(glove, link, destinationPage);
             if (similarity > highestSimilarity) {
                 highestSimilarity = similarity;
                 sourcePage = link;
             }
         }
-        pagesPath.push(sourcePage.toLowerCase());
+        pagesPath.push(sourcePage);
         console.log(sourcePage + " is the best page with a similarity of " + highestSimilarity);
         console.log();
         await new Promise(r => setTimeout(r, 1000));
